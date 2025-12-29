584b3a9f4b65229eaf8d9b7f5f370351ec5b51fa
Al Romero <jrromero0729@gmail.com>
2025-12-29 12:28:34 -0500

    Update verification.ps1

--- WN11-SO-000280-minimum-password-age/verification.ps1
+++ WN11-SO-000280-minimum-password-age/verification.ps1
@@ -6,14 +6,14 @@
 
 .NOTES
     Author          : Albert Romero
-    Date Created    : 2025-12-27
-    Last Modified   : 2025-12-27
+    Date Created    : 2025-12-19
+    Last Modified   : 2025-12-29
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A (Tenable Audit STIG ID: WN11-SO-000280)
 
 .TESTED ON
-    Date(s) Tested  : 2025-12-27
+    Date(s) Tested  : 2025-12-20, 2025-12-28
     Tested By       : Albert Romero
     Systems Tested  : Windows 11
     PowerShell Ver. : 5.1
