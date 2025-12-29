ab954e5876e4f92a969c8b07e102f1e2ec7c0bd2
Al Romero <jrromero0729@gmail.com>
2025-12-29 11:55:14 -0500

    Update remediation.ps1

--- WN11-00-000135-enable-host-based-firewall/remediation.ps1
+++ WN11-00-000135-enable-host-based-firewall/remediation.ps1
@@ -6,14 +6,14 @@
 
 .NOTES
     Author          : Albert Romero
-    Date Created    : 2025-12-27
-    Last Modified   : 2025-12-27
+    Date Created    : 2025-10-03
+    Last Modified   : 2025-12-29
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A (Tenable Audit STIG ID: WN11-00-000135)
 
 .TESTED ON
-    Date(s) Tested  : 2025-12-27
+    Date(s) Tested  : 2025-10-04, 2025-12-27
     Tested By       : Albert Romero
     Systems Tested  : Windows 11
     PowerShell Ver. : 5.1
